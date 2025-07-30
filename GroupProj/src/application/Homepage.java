1d4a3dedda7045c04733a6e27106a9ce7f67f424
Michelle <michelle.lima.284@my.csun.edu>
2025-07-30 01:20:53 -0700

    SCRUM-12: Added frontend source files

---
+++ GroupProj/src/application/Homepage.java
@@ -0,0 +1,24 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Homepage extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/application/Homepage.fxml"));
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("GeekCaps");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
