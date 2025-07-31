94f646545e4251d08cc88336f1ff60239a407ea7
Nery Armaz <neryarmaz@gmail.com>
2025-07-30 22:42:37 -0700

    Updated timeline

--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-module GroupProj {
-	requires javafx.controls;
-	requires javafx.graphics;
-	requires javafx.fxml;
-	requires javafx.base;
-	
-	opens application to javafx.fxml;
-	exports application;
-}
