28bd276e70371a9c2749cee97268b6890be1f007
Nery Armaz <neryarmaz@gmail.com>
2025-08-05 17:45:24 -0700

    Remove module-info.java to disable modules

--- CS380Proj/src/application/Controller.java
+++ CS380Proj/src/application/Controller.java
@@ -4,8 +4,16 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.beans.binding.Bindings;
+import javafx.fxml.Initializable;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 
-public class Controller {
+public class Controller implements Initializable{
 
     @FXML private Button btnKeyboards;
     @FXML private MenuButton btnKeycaps;
@@ -13,7 +21,17 @@ public class Controller {
     @FXML private Button btnCart;
     @FXML private Button btnAccount;
     @FXML private TextField searchField;
-
+    @FXML private HBox mainImageContainer;
+    @FXML private ImageView mainImageView;
+    @FXML private HBox secondContainer;
+    @FXML private ImageView secondImageView;
+    @FXML private HBox thirdContainer;
+    @FXML private ImageView thirdImageView;
+    @FXML private HBox bottomImageRow;
+    @FXML private VBox mainVBox;
+    
+    
+    //buttons
     @FXML
     private void handleCartClick() {
         System.out.println("Cart clicked!");
@@ -37,5 +55,28 @@ public class Controller {
     @FXML
     private void handleSwitchesClick() {
         System.out.println("Switches clicked!");
+    }
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    	mainImageView.fitWidthProperty().bind(mainImageContainer.widthProperty());
+    	mainImageView.fitHeightProperty().bind(mainImageContainer.heightProperty());
+    	
+    	secondImageView.fitWidthProperty().bind(secondContainer.widthProperty());
+    	secondImageView.fitHeightProperty().bind(secondContainer.heightProperty());
+    	
+    	thirdImageView.fitWidthProperty().bind(thirdContainer.widthProperty());
+    	thirdImageView.fitHeightProperty().bind(thirdContainer.heightProperty());
+    	
+    	VBox.setVgrow(mainImageContainer,  Priority.ALWAYS);
+    	VBox.setVgrow(bottomImageRow,  Priority.SOMETIMES);
+    	HBox.setHgrow(secondContainer, Priority.ALWAYS);
+    	HBox.setHgrow(thirdContainer, Priority.ALWAYS);
+    	;
+    	
+    }
+    
+    private double clamp(double value, double min, double max) {
+    	return Math.max(min, Math.min(value,  max));
     }
 }
