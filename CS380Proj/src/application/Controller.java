36a6a2305bd15a507de1838050754eacdc6e8fda
Nery Armaz <neryarmaz@gmail.com>
2025-08-10 20:14:48 -0700

    SCRUM-22  Added keyboard and login fxml pages

--- CS380Proj/src/application/Controller.java
+++ CS380Proj/src/application/Controller.java
@@ -24,7 +24,7 @@ import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class Controller /*implements Initializable*/{
+public class Controller{
 
     @FXML private Button btnKeyboards;
     @FXML private MenuButton btnKeycaps;
@@ -32,14 +32,6 @@ public class Controller /*implements Initializable*/{
     @FXML private Button btnCart;
     @FXML private Button btnAccount;
     @FXML private TextField searchField;
-    //@FXML private HBox mainImageContainer;
-    //@FXML private ImageView mainImage;
-    /*@FXML private HBox secondContainer;
-    @FXML private ImageView secondImageView;
-    @FXML private HBox thirdContainer;
-    @FXML private ImageView thirdImageView;
-    @FXML private HBox bottomImageRow;
-    @FXML private VBox mainVBox;*/
     
     private Stage stage;
     private Scene scene;
@@ -59,40 +51,21 @@ public class Controller /*implements Initializable*/{
     @FXML
     private void handleAccountClick(ActionEvent event) {
         System.out.println("Account clicked!");
-        
-        try {
-			Parent root = FXMLLoader.load(getClass().getResource("LoginPage.fxml"));
-			stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-			scene = new Scene(root);
-			stage.setScene(scene);
-			stage.show();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+        createScene(event, "LoginPage.fxml");
         
     }
     
     @FXML
     private void handleBackToHomepageClick(ActionEvent event) {
         System.out.println("Back!");
-        
-        try {
-			Parent root = FXMLLoader.load(getClass().getResource("Homepage.fxml"));
-			stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-			scene = new Scene(root);
-			stage.setScene(scene);
-			stage.show();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+        createScene(event, "Homepage.fxml");
         
     }
 
     @FXML
-    private void handleKeyboardsClick() {
+    private void handleKeyboardsClick(ActionEvent event) {
         System.out.println("Keyboards clicked!");
+        createScene(event, "keyboardPage.fxml");
     }
 
     @FXML
@@ -104,43 +77,23 @@ public class Controller /*implements Initializable*/{
     private void handleSwitchesClick() {
         System.out.println("Switches clicked!");
         //store s = new store();
-        product p = new product();
+        //product p = new product();
         
  
     }
     
     
-    /*
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-    	mainImage.fitWidthProperty().bind(mainImageContainer.widthProperty());
-    	mainImage.fitHeightProperty().bind(mainImageContainer.heightProperty());
-    	
-    	secondImageView.fitWidthProperty().bind(secondContainer.widthProperty());
-    	secondImageView.fitHeightProperty().bind(secondContainer.heightProperty());
-    	
-    	thirdImageView.fitWidthProperty().bind(thirdContainer.widthProperty());
-    	thirdImageView.fitHeightProperty().bind(thirdContainer.heightProperty());
-    	
-    	HBox.setHgrow(mainImageContainer,  Priority.ALWAYS);
-    	VBox.setVgrow(bottomImageRow,  Priority.SOMETIMES);
-    	HBox.setHgrow(secondContainer, Priority.ALWAYS);
-    	HBox.setHgrow(thirdContainer, Priority.ALWAYS);
-    	
-    }*/
-
-    
-    private double clamp(double value, double min, double max) {
-    	return Math.max(min, Math.min(value,  max));
+    private void createScene(ActionEvent eve,String fileName) {
+    	try {
+			Parent root = FXMLLoader.load(getClass().getResource(fileName));
+			stage = (Stage)((Node)eve.getSource()).getScene().getWindow();
+			scene = new Scene(root);
+			stage.setScene(scene);
+			stage.show();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
 }
