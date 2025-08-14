6284a7e5b810feca44f5bb2890a16d3be341df88
Matt <mberleson001@gmail.com>
2025-08-13 20:39:07 -0700

    SCRUM-23 Fixing switch page to select products

--- CS380Proj/src/application/ProductItemController.java
+++ CS380Proj/src/application/ProductItemController.java
@@ -1,8 +1,11 @@
 package application;
 
+import Company.products;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 public class ProductItemController {
 
@@ -15,19 +18,38 @@ public class ProductItemController {
 
     @FXML
     private Label productLabel;
+    
+    @FXML
+    private void click(MouseEvent mouseEvent) {
+    	clickListener.onActionListener(item);
+    }
+    
+    private InterfaceListener clickListener;
+    private products.product item;
 
-
-    public void setProductName(String name) {
-        productLabel.setText(name);
+    public void setData(products.product product, InterfaceListener listener) {
+        this.item = product;
+        this.clickListener = listener;
+        
+        productLabel.setText(product.getName());
+        priceLabel.setText("$" + product.getPrice());
+        
+        // Assuming the image is available as a resource
+        Image img = new Image(getClass().getResourceAsStream(product.getImgSrc()));
+        productImg.setImage(img);
     }
 
-    public void setPrice(String price) {
-        priceLabel.setText(price);
-    }
-
-    public void setImage(javafx.scene.image.Image image) {
-        productImg.setImage(image);
-    }
+//    public void setProductName(String name) {
+//        productLabel.setText(name);
+//    }
+//
+//    public void setPrice(String price) {
+//        priceLabel.setText(price);
+//    }
+//
+//    public void setImage(javafx.scene.image.Image image) {
+//        productImg.setImage(image);
+//    }
 
     
 
